71abb1e48f9c49cf66f2838835b230f65b00ea50
McNova <novacrash_@hotmail.com>
2015-04-02 17:45:13 -0400

    Update Adafruit_MCP23008.cpp

--- firmware/Adafruit_MCP23008.cpp
+++ firmware/Adafruit_MCP23008.cpp
@@ -1,17 +1,16 @@
 #include "Adafruit_MCP23008.h"
 
-/***************************************************
-  This is a library for the MCP23008 i2c port expander
 
-  These displays use I2C to communicate, 2 pins are required to
-  interface
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
+//  This is a library for the MCP23008 i2c port expander
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
+//  These displays use I2C to communicate, 2 pins are required to
+//  interface
+//  Adafruit invests time and resources providing this open source code,
+//  please support Adafruit and open-source hardware by purchasing
+//  products from Adafruit!
+
+//  Written by Limor Fried/Ladyada for Adafruit Industries.
+//  BSD license, all text above must be included in any redistribution
 
 ////////////////////////////////////////////////////////////////////////////////
 // RTC_DS1307 implementation
